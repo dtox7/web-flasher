dc01c91e4ad84ea81d15add2db9b07ad03321545
dtox7 <155916814+dtox7@users.noreply.github.com>
2024-02-28 21:58:20 +0100

    Ok

--- components/Device.vue
+++ components/Device.vue
@@ -9,7 +9,7 @@
         <div id="dropdownDevices" class="z-10 hidden bg-gray-200 divide-y divide-gray-500 rounded-lg shadow w-52">
             <ul class="py-2 text-sm text-gray-900" aria-labelledby="dropdownDeviceButton">
                 <li v-for="target in store.$state.targets.sort((a, b) => a.displayName.localeCompare(b.displayName))">
-                    <a class="block px-4 py-1 hover:bg-gray-400 cursor-pointer" @click="setTarget(target)">
+                    <a class="block px-4 py-1 hover:bg-gray-400 cursor-pointer" @click="store.autoSelectHardware">
                         {{ target.displayName }}
                     </a>
                 </li>
@@ -17,7 +17,8 @@
         </div>
         <button data-tooltip-target="tooltip-auto" class="mx-2 display-inline content-center px-3 py-2 text-xs font-medium text-center  hover:bg-gray-200 focus:ring-4 focus:outline-none focus:ring-blue-300 rounded-lg inline-flex items-center text-white hover:text-black"
             type="button"
-            @click="store.autoSelectHardware">
+            
+            @click="setTarget(target)">
             <SparklesIcon class="h-4 w-4 " />
         </button>
         <div id="tooltip-auto" role="tooltip" class="absolute z-10 invisible inline-block px-3 py-2 text-sm font-medium text-white transition-opacity duration-300  rounded-lg shadow-sm opacity-0 tooltip bg-gray-700">
@@ -37,7 +38,7 @@ import { useDeviceStore } from '../stores/deviceStore';
 const store = useDeviceStore();
 store.fetchList();
 
-// const selectedTarget = computed(() => store.$state.selectedTarget?.hwModel ? store.$state.selectedTarget?.displayName : "Select Target Device")
+//const selectedTarget = computed(() => store.$state.selectedTarget?.hwModel ? store.$state.selectedTarget?.displayName : "Select Target Device")
 
 const setTarget = (target: DeviceHardware) => {
     store.setSelectedTarget(target);
